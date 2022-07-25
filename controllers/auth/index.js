e6b277cafbb627d068821dec7487b06ba164ef5c
Tno-MSA <tno.rappelz@gmail.com>
2022-07-25 23:59:19 +0300

    added verifyCode

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,5 +2,6 @@ import register from "./register.js";
 import login from "./login.js";
 import logout from "./logout.js";
 import verifyToken from "./verifyToken.js";
+import verifyCode from "./verifyCode.js";
 
-export default { register, login, logout, verifyToken };
+export default { register, login, logout, verifyToken, verifyCode };
