228c56d4caea38b957b20e146dc8a79e1593c4da
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 16:50:36 +0300

    added "res.locals" and "backup res"

--- controllers/auth/verifyToken.js
+++ controllers/auth/verifyToken.js
@@ -1,13 +1,19 @@
-import jwt from "jsonwebtoken";
 import { cookiesConfig } from "../../configs/index.js";
 
 export default async function verifyToken(req, res) {
-  return res.status(200).json({
+  const locals = res.locals.accessToken;
+  const backupResponse = {
     statusCode: 200,
+    isAuth: true,
     from: "controllers/auth/verifyToken 1",
     message: "all good.",
     data: {
       accessToken: cookiesConfig.access.name,
     },
+  };
+  const response = locals ? locals : backupResponse;
+
+  return res.status(200).json({
+    ...response,
   });
 }
