ce16cf9156142c6577b8026b825d210f03b669e8
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 16:45:28 +0300

    added "genImageURL"

--- utils/helpers/index.js
+++ utils/helpers/index.js
@@ -1,5 +1,6 @@
 import genBcrypt from "./genBcrypt.js";
 import verifyBcrypt from "./verifyBcrypt.js";
 import verifyJWT from "./verifyJWT.js";
+import genImageURL from "./genImageURL.js";
 
-export default { genBcrypt, verifyBcrypt, verifyJWT };
+export default { genBcrypt, verifyBcrypt, verifyJWT, genImageURL };
