7556387f715b8d6fb67da4746ce21026e5c1b3a3
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 01:40:17 +0300

    added "comment" and "recommendations" routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 import auth from "./auth.routes.js";
 import memory from "./memory.routes.js";
+import comment from "./comment.routes.js";
+import recommendations from "./recommendations.routes.js";
 
-export default { auth, memory };
+export default { auth, memory, comment, recommendations };
