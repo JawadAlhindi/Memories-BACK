787261a52a84b5ae9b3610a6df78716644a7be1a
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 02:25:48 +0300

    added "commentCons"

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 import authCons from "./auth/index.js";
 import memoryCons from "./memory/index.js";
+import commentCons from "./comment/index.js";
 
-export { authCons, memoryCons };
+export { authCons, memoryCons, commentCons };
