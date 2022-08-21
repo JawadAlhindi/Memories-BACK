a6bdc622453e6d5c29bd3ebf581f780622c1e51c
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 01:37:14 +0300

    moved "cloudinary" to separate file

--- services/cloudinary/upload.js
+++ services/cloudinary/upload.js
@@ -1,12 +1,7 @@
-import { v2 as cloudinary } from "cloudinary";
-import configs from "../../configs/cloudinary.js";
-
-cloudinary.config(configs);
+import base from "./options.js";
 
 export default async function upload(payload) {
-  const { public_id } = await cloudinary.uploader.upload(payload, {
-    format: "webp",
-  });
+  const { public_id } = await base.upload(payload, { format: "webp" });
 
   return public_id;
 }
