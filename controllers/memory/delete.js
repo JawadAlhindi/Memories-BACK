b24d6a4f3e6f69d398df98b480cea9b8f7b374aa
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 02:25:38 +0300

    moved the backup to separate file

--- controllers/memory/delete.js
+++ controllers/memory/delete.js
@@ -1,15 +1,30 @@
 import { memoryModel } from "../../models/index.js";
+import { cookiesConfig } from "../../configs/index.js";
+import { cloudinary } from "../../services/index.js";
 
 export default async function _delete(req, res) {
-  const { _id } = req.body;
+  const { _id, public_id } = req.body;
+
+  const localsAccessToken = res.locals.accessToken;
+  const backupResponse = {
+    statusCode: 200,
+    isAuth: true,
+    from: "controllers/memory/delete",
+    message: "all good.",
+    data: {
+      accessToken: cookiesConfig.access.name,
+    },
+  };
+  const response = localsAccessToken ? localsAccessToken : backupResponse;
 
   try {
+    await cloudinary.destory(public_id);
     await memoryModel.findByIdAndRemove(_id);
 
-    return res.status(204).json({
-      accessToken: res.locals.accessToken,
+    return res.status(200).json({
+      accessToken: response,
       memory: {
-        statusCode: 204,
+        statusCode: 200,
         from: "controllers/memory/delete 1",
         message: "Your memory has been successfully deleted.",
       },
@@ -17,7 +32,7 @@ export default async function _delete(req, res) {
   } catch (error) {
     console.log(error);
     return res.status(503).json({
-      accessToken: res.locals.accessToken,
+      accessToken: response,
       memory: {
         statusCode: 503,
         from: "controllers/memory/delete 2",
