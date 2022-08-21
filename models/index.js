cc5b374ba9cb7d1da37a0085de80953c953c2c69
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 01:40:53 +0300

    added "commentModel"

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 import userModel from "./user.js";
 import memoryModel from "./memory.js";
+import commentModel from "./comment.js";
 
-export { userModel, memoryModel };
+export { userModel, memoryModel, commentModel };
