805a3a26f48a89c2cf11677f9bbc69079731e586
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 01:43:52 +0300

    moved "backup" to separate file, re-arrange

--- controllers/memory/like.js
+++ controllers/memory/like.js
@@ -1,18 +1,39 @@
 import { memoryModel } from "../../models/index.js";
 import { helpers } from "../../utils/index.js";
+import { cookiesConfig } from "../../configs/index.js";
 
 export default async function like(req, res) {
-  const { _id, userId } = req.body;
-  const memory = {};
-  const index = 0;
-  const updatedMemory = {};
+  const { _id, userId, type } = req.body;
+  const isCard = type === "card";
+  let memory = {};
+
+  const localsAccessToken = res.locals.accessToken;
+  const backupResponse = {
+    statusCode: 200,
+    isAuth: true,
+    from: "controllers/memory/like",
+    message: "all good.",
+    data: {
+      accessToken: cookiesConfig.access.name,
+    },
+  };
+  const response = localsAccessToken ? localsAccessToken : backupResponse;
 
   try {
     memory = await memoryModel.findById(_id).lean();
+
+    const index = await memory.likes.findIndex(
+      (id) => id.toString() === userId
+    );
+    if (index === -1) {
+      memory.likes.push(userId);
+    } else {
+      memory.likes = memory.likes.filter((id) => id.toString() !== userId);
+    }
   } catch (error) {
     console.log(error);
     return res.status(503).json({
-      accessToken: res.locals.accessToken,
+      accessToken: response,
       memory: {
         statusCode: 503,
         from: "controllers/memory/like 1",
@@ -22,38 +43,18 @@ export default async function like(req, res) {
   }
 
   try {
-    index = await memory.likes.findIndex((id) => id.toString() === userId);
-  } catch (error) {
-    console.log(error);
-    return res.status(503).json({
-      accessToken: res.locals.accessToken,
-      memory: {
-        statusCode: 503,
-        from: "controllers/memory/like 2",
-        message: "Something went wrong! Please try again.",
-      },
-    });
-  }
-
-  if (index === -1) {
-    memory.likes.push(userId);
-  } else {
-    memory.likes = memory.likes.filter((id) => id.toString() !== userId);
-  }
-
-  try {
-    updatedMemory = await memoryModel
+    const updatedMemory = await memoryModel
       .findByIdAndUpdate(_id, memory, { new: true })
       .populate("author", "username avatar")
       .lean();
 
-    updatedMemory.cover = helpers.genImageURL(
+    updatedMemory.coverURL = helpers.genImageURL(
       updatedMemory.cover,
-      "c_scale,h_420/q_auto:best/dpr_auto"
+      `c_scale,${isCard ? "h_420" : "h_1024"}/q_auto:best/dpr_auto`
     );
 
     return res.status(200).json({
-      accessToken: res.locals.accessToken,
+      accessToken: response,
       memory: {
         statusCode: 200,
         from: "controllers/memory/likeMemory",
@@ -65,7 +66,7 @@ export default async function like(req, res) {
   } catch (error) {
     console.log(error);
     return res.status(503).json({
-      accessToken: res.locals.accessToken,
+      accessToken: response,
       memory: {
         statusCode: 503,
         from: "controllers/memory/like 3",
