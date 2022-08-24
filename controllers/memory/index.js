5a2c29fd58f190e4111e59d8a047b799f2c13881
Tno-MSA <tno.rappelz@gmail.com>
2022-08-24 23:41:00 +0300

    added "getAllLikes"

--- controllers/memory/index.js
+++ controllers/memory/index.js
@@ -1,5 +1,6 @@
 import create from "./create.js";
 import getAll from "./getAll.js";
+import getAllLikes from "./getAllLikes.js";
 import getSingle from "./getSingle.js";
 import getTags from "./getTags.js";
 import _delete from "./delete.js";
@@ -11,6 +12,7 @@ import getRecommendations from "./getRecommendations.js";
 export default {
   create,
   getAll,
+  getAllLikes,
   getSingle,
   getTags,
   getRecommendations,
